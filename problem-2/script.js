739c8055e8865753ff5eceb87940d96bbf162f03
Dalton Otineru <d@dmbp.local>
2021-11-12 15:06:21 -0800

    fixed

--- problem-2/script.js
+++ problem-2/script.js
@@ -1,7 +1,3 @@
-// define a variable thats holding an array of intergers
-// define a variable that contains the function of adding up the sum
-// use the .reduce method() on the array passed in
-
 function sumArray(arr){
     if(arr.length === 1){
         return arr[0];
